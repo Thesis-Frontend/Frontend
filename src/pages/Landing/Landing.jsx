512f356300c9216cd2e3db050f8efe18d63c23b8
Sude Nur Çevik <sudenur867@gmail.com>
2024-04-19 15:21:48 +0300

    landing page update

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
-import bg from "../../assets/First_screen_picture.jpg";
 import Login from "../Login/Login";
+import "./Landing.css";
+
+import leftImage from "../../assets/left.jpg";
+import rightImage1 from "../../assets/Shape1.jpg";
+import rightImage2 from "../../assets/Shape2.jpg";
 
 export default function Landing() {
   const [showModal, setShowModal] = useState(false);
@@ -18,13 +22,30 @@ export default function Landing() {
 
   return (
     <div className="relative min-h-screen">
-      <Navbar setLoginModal={handleLoginClick} setIsUserLogin={setIsUserLogin} />
-      <div
-        className="absolute inset-0 bg-cover bg-center"
-        style={{ backgroundImage: `url(${bg})`, height: "100vh" }}
-      >
+      <Navbar
+        setLoginModal={handleLoginClick}
+        setIsUserLogin={setIsUserLogin}
+      />
+      <div className="absolute inset-0 flex items-center justify-centers pb-20">
+        <img
+          src={leftImage}
+          alt="Left Image"
+          className="absolute left-0 h-auto w-2/5"
+          style={{top: "11%"}}
+        />
+        <img
+          src={rightImage1}
+          alt="Right Image 1"
+          className="absolute bottom-0 right right-image"
+        />
+        <img
+          src={rightImage2}
+          alt="Right Image 2"
+          className="absolute"
+          style={{right: "0", width: "17%", top:"20%"}}
+        />
         <p
-          className="absolute p-4 text-3xl font-bold tracking-wide"
+          className="absolute p-4 text-3xl font-bold tracking-wide text-white"
           style={{
             right: "15%",
             lineHeight: "2",
@@ -41,7 +62,11 @@ export default function Landing() {
       <div className="absolute bottom-0 w-full">
         <Footer />
       </div>
-      <Login isOpen={showModal} onClose={handleCloseModal} isUserLogin={isUserLogin} />
+      <Login
+        isOpen={showModal}
+        onClose={handleCloseModal}
+        isUserLogin={isUserLogin}
+      />
     </div>
   );
 }
