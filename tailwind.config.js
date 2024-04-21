d5b3c4fbda00d8f7d2a4dd24a5e339ab7495d61f
talhaarik <talhaarikyatirim@gmail.com>
2024-04-21 22:21:06 +0300

    Color and font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,26 @@ export default {
   theme: {
     extend: {
       colors: {
-        landingBackgroundColor: "#f3f3f3", // Example color code
+        landingBackgroundColor: "#F0F0F0", // Kaydol kısmının arka planı, butonları ve Login butonları için de aynı
+        landingFontColor: "#000000", // Bazı textlerin pass throughları farklı bakarız tasarımdan.
+        landingButtonTextColor: "#FFFFFF",
+        landingShapeColor: "#D9D9D9", // Kaydol kısmının shapeler ve kaydol üst buton için de aynı
+        signupCardColor: "#757575",  // Kaydol kısmının textleri için de aynı renk
+        signupButtonStrokeColor: "#5C5E64", // Butonların strokeları, alt geçiş butonlarının renkleri (Ana sayfada navbar ve sidebardaki textler ve ikonlar) 
+        cardBackground: "#2B3245",
+        cardBronze: "#FF6038",
+        cardGold: "#FFB828",
+        cardSilver: "#2E69FF",
+        loginSuccess: "#4BB543",  // Video screende ilerle butonu rengi
+        loginUnsuccess: "#FC100D", // pass throughları farklı
+        createButtons: "#FF8C00",
+        updateButton: "#1974D2",
+        videoScreenShapes: "#7929FF",
       },
+      fontFamily: {
+        inter: ["Inter", 'sans-serif'],
+        open: ['Open Sans', 'sans-serif'],
+      }
     },
   },
   plugins: [],
