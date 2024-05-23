f3c5e9d4ac020f6387ee9bcbf46e2527b15e6639
Sude Nur Çevik <sudenur867@gmail.com>
2024-05-23 23:51:51 +0300

    loading indicator updates

--- src/pages/Role&Auth/Policy/PolicyEditor.jsx
+++ src/pages/Role&Auth/Policy/PolicyEditor.jsx
@@ -229,13 +229,7 @@ const PolicyEditor = () => {
       </div>
       <div className="flex justify-between items-center mb-4">
         {isDownloading ? (
-          <svg
-            className="h-8 w-8 animate-spin self-end"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-          >
-            <path d="M304 48a48 48 0 1 0 -96 0 48 48 a 1 0 0 0 96 0zM304 416 a 48 48 0 1 0 -96 0 48 48 a 1 0 0 0 96 0zM48 304a48 48 a 1 0 0 0 0-96 48 48 0 1 0 0 96zm464-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM142.9 437A48 48 0 1 0 75 369.1 48 48 0 1 0 142.9 437zm0-294.2A48 48 0 1 0 75 75a48 48 0 1 0 67.9 67.9zM369.1 437A48 48 0 1 0 437 369.1 48 48 0 1 0 369.1 437z" />
-          </svg>
+          <div className="loader"></div>
         ) : (
           <button
             className="bg-orange-500 text-white px-4 py-2 rounded-md hover:bg-orange-600"
